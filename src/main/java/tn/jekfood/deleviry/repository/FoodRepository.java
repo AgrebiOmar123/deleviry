e80d6df68101b9d6270f85267dd49dc5fd8fa297
AgrebiOmar123 <oagrebi614@gmail.com>
2023-11-19 10:53:51 +0100

    deleviry spring boot

---
+++ src/main/java/tn/jekfood/deleviry/repository/FoodRepository.java
@@ -0,0 +1,13 @@
+package tn.jekfood.deleviry.repository;
+import org.springframework.data.repository.CrudRepository;
+import tn.jekfood.deleviry.model.Food;
+public interface FoodRepository extends CrudRepository<Food, Integer>  {
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/tn/jekfood/deleviry/repository/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package tn.jekfood.deleviry.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import tn.jekfood.deleviry.model.Restaurant;
+public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
+
+}
